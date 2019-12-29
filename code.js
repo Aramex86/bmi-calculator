0340243f6fb840e8cb511cd0b6b3f133ff13e144
Slava <slavbez@mail.ru>
2019-12-29 15:01:35 +0200

    BMI Calculator

---
+++ code.js
@@ -0,0 +1,44 @@
+
+function getResult(weightPerson,heightPerson){
+
+    //Take the value from input's
+    let name = document.getElementById('name').value;
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+    //Convert values in nembers
+    const weightConv = Number(weight);
+    const heightConv = Number(height);
+
+    //Do the calculations
+    weightPerson = weight;
+    heightPerson = height;
+
+   var BMI =  weightPerson / heightPerson;
+
+   //Print the result
+
+    const result = `Hello ${name}, your BMI is ${Math.round(BMI)}`;
+
+    let resultBMI = document.querySelector('.result');
+     
+    resultBMI.innerHTML = result;
+    
+     
+     
+}
+
+getResult();
+
+
+
+
+
+
+
+
+
+
+
+
+
